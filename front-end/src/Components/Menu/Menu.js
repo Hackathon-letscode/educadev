569245d71ec19dbe5415a9e383e3770545aa075c
Thales Vanzo Vieira <thales.v.vieira@gmail.com>
2022-07-29 02:58:45 -0300

    layout home

--- front-end/src/Components/Menu/Menu.js
+++ front-end/src/Components/Menu/Menu.js
@@ -21,7 +21,7 @@ function NavScrollExample() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            // style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link to="/home" href="home">
